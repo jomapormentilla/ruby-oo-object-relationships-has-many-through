9f5417a125d4d14a6e6b5bd5bff8c20da9bbfcd4
jomapormentilla <jomapormentilla@gmail.com>
2020-10-27 23:29:25 -0400

    commit tree 0fb7ad37a62f1dbb27cf8532b2e54d2c914eef08

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Customer
   
+    @@all = []
+
+    def initialize( name, age )
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal( waiter, total, tip=0 )
+        Meal.new( waiter, self, total, tip )
+    end
+
+    def waiters
+        meals.map do |meal|
+            meal.waiter
+        end
+    end
+
+    def meals
+        arr = []
+        Meal.all.each do |meal|
+            if meal.customer == self
+                arr << meal
+            end
+        end
+        arr
+    end
 end
